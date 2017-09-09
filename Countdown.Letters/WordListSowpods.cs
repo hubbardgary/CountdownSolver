6c88de13fb2c783f9fd3283a7f654f14cf9dfa45
Gary Hubbard <hubbardgary@users.noreply.github.com>
2017-09-09 14:58:24 +0100

    Fix path to dictionary file

--- Countdown.Letters/WordListSowpods.cs
+++ Countdown.Letters/WordListSowpods.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 
 namespace Countdown.Letters
 {
     public class WordListSowpods : IWordList
     {
-        private const string _filename = @"dictionaries\sowpods.txt";
+        private readonly string _filename = $"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}/dictionaries/sowpods.txt";
         private readonly IEnumerable<string> _permittedWords;
 
         public WordListSowpods()

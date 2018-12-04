4b371a7b9a77dd19abf569ac39c561fef26a935f
Gary Hubbard <hubbardgary@gmail.com>
2018-12-04 07:34:17 +0000

    Add HealthCheckController

---
+++ Api/Countdown.Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Countdown.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class HealthCheckController : Controller
+    {
+        // GET api/healthcheck/heartbeat
+        [Route("heartbeat")]
+        public ActionResult Heartbeat()
+        {
+            return Ok("alive");
+        }
+    }
+}
